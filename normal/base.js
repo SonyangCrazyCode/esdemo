b10d90c31dcdd3595dfa856f60dfe3c79faaa3d2
sonyang@sohu.com <sonyang@sohu.com>
2018-03-14 00:19:20 +0800

    modify

--- normal/base.js
+++ normal/base.js
@@ -1,17 +1,38 @@
-class test{
-    constructor()
-    {
-      this.a=10;
-      this.b=20;     
-    }
-    toString()
-    {
-      return "test.class";
-    }
-    func(c) {
-      return c;
-    }
+class animal{
+  constructor(name)
+  {
+    this.name=name;
+    this.age=1;     
+  }
+  say()
+  {
+    return this.name+" say";
+  }
+  walk()
+  {
+    return this.name+" walk";
+  }
 }
 
+class dog extends animal
+{
+  constructor(name)
+  {
+    super(name);
+    this.name=name;
+  }
+  helpPerson(...per)
+  {
+    let behavior="";
+    for(let p of per)    
+    {
+      behavior+=this.name+" help "+ p +",";
+    }
+    return behavior.substring(0,behavior.length-1);
+  }
+}
 
-
+function fun1()
+{
+    return "fun1";
+}
