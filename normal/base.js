3175cf73c32ab6287d458c00e4bd6298c6e4553d
sonyang@sohu.com <sonyang@sohu.com>
2018-03-13 23:03:20 +0800

    Signed-off-by: sonyang@sohu.com <sonyang@sohu.com>

---
+++ normal/base.js
@@ -0,0 +1,17 @@
+class test{
+    constructor()
+    {
+      this.a=10;
+      this.b=20;     
+    }
+    toString()
+    {
+      return "test.class";
+    }
+    func(c) {
+      return c;
+    }
+}
+
+
+
